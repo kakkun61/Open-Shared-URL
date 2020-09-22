0ee5479eac6696c2d42d1481bac77ab897f86047
Kazuki Okamoto <kazuki.okamoto@kakkun61.com>
2020-09-23 05:11:53 +0900

    new version

--- app/src/main/java/com/kakkun61/opensharedurl/DialogActivity.java
+++ app/src/main/java/com/kakkun61/opensharedurl/DialogActivity.java
@@ -3,7 +3,6 @@ package com.kakkun61.opensharedurl;
 import java.util.Collections;
 import java.util.List;
 
-import android.app.ListActivity;
 import android.content.ComponentName;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
@@ -12,70 +11,91 @@ import android.content.pm.ResolveInfo;
 import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
-public class DialogActivity extends ListActivity {
+import androidx.appcompat.app.AppCompatActivity;
+
+public class DialogActivity extends AppCompatActivity {
     private AppAdapter adapter;
     private Uri uri;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_dialog);
 
         Bundle extras = getIntent().getExtras();
-        if (extras == null) return;
 
-        uri = Uri.parse(extras.getString(Intent.EXTRA_TEXT));
+        String uriString = extras.getString(Intent.EXTRA_TEXT);
+
+        if (TextUtils.isEmpty(uriString)) {
+            setContentView(R.layout.activity_dialog_empty);
+            TextView message = findViewById(R.id.message);
+            message.setText(R.string.not_url);
+            return;
+        }
+
+        setTitle(uriString);
+
+        uri = Uri.parse(uriString);
 
         PackageManager pm = getPackageManager();
         Intent urlIntent = new Intent(Intent.ACTION_VIEW, uri);
         List<ResolveInfo> resolveInfos = pm.queryIntentActivities(urlIntent, 0);
         Collections.sort(resolveInfos, new ResolveInfo.DisplayNameComparator(pm));
 
+        if (resolveInfos.isEmpty()) {
+            setContentView(R.layout.activity_dialog_empty);
+            TextView message = findViewById(R.id.message);
+            message.setText(R.string.no_app);
+            return;
+        }
+
+        setContentView(R.layout.activity_dialog);
+        ListView listView = findViewById(R.id.list);
         adapter = new AppAdapter(pm, resolveInfos);
-        setListAdapter(adapter);
-    }
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView l, View v, int position, long id) {
+                ResolveInfo resolveInfo = adapter.getItem(position);
 
-    @Override
-    protected void onListItemClick(ListView l, View v, int position, long id) {
-        ResolveInfo resolveInfo = adapter.getItem(position);
-        if (resolveInfo == null) return;
+                ActivityInfo activityInfo = resolveInfo.activityInfo;
+                ComponentName name = new ComponentName(activityInfo.applicationInfo.packageName, activityInfo.name);
 
-        ActivityInfo activityInfo = resolveInfo.activityInfo;
-        ComponentName name = new ComponentName(activityInfo.applicationInfo.packageName, activityInfo.name);
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(uri);
+                intent.setComponent(name);
 
-        Intent intent = new Intent(Intent.ACTION_VIEW);
-        intent.setData(uri);
-        intent.setComponent(name);
+                startActivity(intent);
 
-        startActivity(intent);
-
-        finish();
+                finish();
+            }
+        });
     }
 
     private class AppAdapter extends ArrayAdapter<ResolveInfo> {
         private PackageManager pm;
 
         public AppAdapter(PackageManager pm, List<ResolveInfo> infos) {
-            super(DialogActivity.this, R.layout.list_row, infos);
+            super(DialogActivity.this, R.layout.activity_dialog_list, infos);
             this.pm = pm;
         }
 
         @Override
         public View getView(int position, View convertView, ViewGroup parent) {
             if (convertView == null) {
-                convertView = getLayoutInflater().inflate(R.layout.list_row, parent, false);
+                convertView = getLayoutInflater().inflate(R.layout.activity_dialog_list, parent, false);
                 convertView.setTag(convertView.findViewById(R.id.item));
             }
 
             TextView text = (TextView) convertView.getTag();
             ResolveInfo resolveInfo = getItem(position);
-            if (resolveInfo == null) return convertView;
             text.setText(resolveInfo.loadLabel(pm));
             Drawable icon = resolveInfo.loadIcon(pm);
             icon.setBounds(0, 0, 116, 116);
